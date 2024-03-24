d77165461d2b331172fc9f39ac21a3ad3fc926a5
Zeffry Reynando <zeffry.reynando@gmail.com>
2024-03-25 04:45:48 +0700

    Adding meta data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,7 +96,21 @@ export default function Page() {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>CMS Template - Home</title>
+        <meta
+          name="description"
+          content="CMS Template merupakan aplikasi berbasis web yang digunakan untuk mengelola konten website. Aplikasi ini memiliki fitur yang memudahkan pengguna dalam mengelola konten website seperti menambah, mengedit, menghapus konten website."
+        />
+        <meta property="og:title" content="CMS Tempalte" />
+        <meta property="og:type" content="CMS Tempalte" />
+        <meta property="og:image:url" content="https://picsum.photos/400" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:image:alt" content="CMS Tempalte Image" />
+        <meta
+          property="og:description"
+          content="CMS Template merupakan aplikasi berbasis web yang digunakan untuk mengelola konten website. Aplikasi ini memiliki fitur yang memudahkan pengguna dalam mengelola konten website seperti menambah, mengedit, menghapus konten website."
+        />
       </Head>
       <div className="min-h-screen">
         <Stack gap={'lg'}>
